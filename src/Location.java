7f4cceb02c80e4a641d017536949147cdf178519
HrudithL <hrudith.l@gmail.com>
2026-03-08 17:32:48 -0500

    Refactor Location: encapsulate fields, add equals/hashCode, fix toString

--- src/Location.java
+++ src/Location.java
@@ -1,29 +1,41 @@
+import java.util.Objects;
+
 public class Location
 {
-    int sheet;
-    int row;
-    int col;
+    private int sheet;
+    private int row;
+    private int col;
 
     /** Sets x, y, z to the recieved values */
-    public Location(int col,int row,int sheet)
+    public Location(int col, int row, int sheet)
     {
-        this.col    = col;
-        this.row    = row;
-        this.sheet     = sheet;
+        this.col   = col;
+        this.row   = row;
+        this.sheet = sheet;
     }
 
     public int getCol()
     {   return col;   }
 
-
     public int getRow()
     {   return row;   }
-
 
     public int getSheet()
     {   return sheet;   }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Location)) return false;
+        Location l = (Location) o;
+        return col == l.col && row == l.row && sheet == l.sheet;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(col, row, sheet);
+    }
 
     public String toString()
-    {   return "(x=" + col+",y="+row+ ",z="+sheet+ ")";   }
+    {   return "(col=" + col + ",row=" + row + ",sheet=" + sheet + ")";   }
 }
